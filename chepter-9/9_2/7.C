2e5aa9c59d1aabfa72f1466d2bb05911f459d826
vandan91060 <vandanpatel493@gmail.com>
2024-02-10 12:01:40 +0530

    10/2/24

---
+++ chepter-9/9_2/7.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    char i,j;
+    
+    for(i=65;i<=69;i++)
+    {
+        for(j=i;j>=65;j--)
+        {
+        printf("%c",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
